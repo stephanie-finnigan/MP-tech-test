f373fe19455722943761da55d6ed47d4aba6fdf9
Stephanie Finnigan <snoellm@gmail.com>
2023-12-14 18:25:27 +0000

    Moved using statements; Added logging

--- Moonpig.PostOffice.Api/Program.cs
+++ Moonpig.PostOffice.Api/Program.cs
@@ -1,8 +1,10 @@
-﻿namespace Moonpig.PostOffice.Api
-{
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using Microsoft.Extensions.Logging;
 
+namespace Moonpig.PostOffice.Api
+{
     public class Program
     {
         public static void Main(string[] args)
@@ -15,6 +17,14 @@
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+                    webBuilder.ConfigureLogging((hostingContext, builder) =>
+                    {
+                        builder
+                        .AddApplicationInsights()
+                        .AddFilter<ApplicationInsightsLoggerProvider>("SimpleLogger", LogLevel.Debug)
+                        .SetMinimumLevel(LogLevel.Warning);
+                    });
                 });
     }
 }
