e214560dbed0020a80fb3afda164155be581fe4d
Stephanie Finnigan <snoellm@gmail.com>
2023-12-14 13:53:35 +0000

    Removed extra spaces

--- Moonpig.PostOffice.Data/Product.cs
+++ Moonpig.PostOffice.Data/Product.cs
@@ -3,11 +3,7 @@
     public class Product
     {
         public int ProductId { get; set; }
-
-
         public string Name { get; set; }
-
-
         public int SupplierId { get; set; }
     }
 }
--- Moonpig.PostOffice.Data/Supplier.cs
+++ Moonpig.PostOffice.Data/Supplier.cs
@@ -3,11 +3,7 @@
     public class Supplier
     {
         public int SupplierId { get; set; }
-
-
         public string Name { get; set; }
-
-
         public int LeadTime { get; set; }
     }
 }
