b964fbd17aee70a8d232b01a47e065d2d2780fa8
Stephanie Finnigan <snoellm@gmail.com>
2023-12-14 19:36:59 +0000

    Fix formatting of return

--- Moonpig.PostOffice.Data/DbContext.cs
+++ Moonpig.PostOffice.Data/DbContext.cs
@@ -58,18 +58,18 @@ namespace Moonpig.PostOffice.Data
             get
             {
                 return new List<Product>
-                           {
-                               new Product { ProductId = 1, Name = "Greetings Card", SupplierId = 1 },
-                               new Product { ProductId = 2, Name = "Flowers", SupplierId = 2 },
-                               new Product { ProductId = 3, Name = "Soft Toy", SupplierId = 4 },
-                               new Product { ProductId = 4, Name = "Chocolate", SupplierId = 1 },
-                               new Product { ProductId = 5, Name = "Canvas", SupplierId = 1 },
-                               new Product { ProductId = 6, Name = "Plant", SupplierId = 2 },
-                               new Product { ProductId = 7, Name = "Alcohol", SupplierId = 1 },
-                               new Product { ProductId = 8, Name = "Box Set", SupplierId = 7 },
-                               new Product { ProductId = 9, Name = "Frozen Doll", SupplierId = 5 },
-                               new Product { ProductId = 10, Name = "Personalised Tacky Thshrt", SupplierId = 6 },
-                           }.AsQueryable();
+                {
+                    new Product { ProductId = 1, Name = "Greetings Card", SupplierId = 1 },
+                    new Product { ProductId = 2, Name = "Flowers", SupplierId = 2 },
+                    new Product { ProductId = 3, Name = "Soft Toy", SupplierId = 4 },
+                    new Product { ProductId = 4, Name = "Chocolate", SupplierId = 1 },
+                    new Product { ProductId = 5, Name = "Canvas", SupplierId = 1 },
+                    new Product { ProductId = 6, Name = "Plant", SupplierId = 2 },
+                    new Product { ProductId = 7, Name = "Alcohol", SupplierId = 1 },
+                    new Product { ProductId = 8, Name = "Box Set", SupplierId = 7 },
+                    new Product { ProductId = 9, Name = "Frozen Doll", SupplierId = 5 },
+                    new Product { ProductId = 10, Name = "Personalised Tacky Thshrt", SupplierId = 6 },
+                }.AsQueryable();
             }
         }
     }
