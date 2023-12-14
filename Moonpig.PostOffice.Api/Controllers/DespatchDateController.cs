c5192108cc69ae335c981807a2322283457fae55
Stephanie Finnigan <snoellm@gmail.com>
2023-12-14 12:14:25 +0000

    Removed code logic from Controller endpoint method
    Moved usings above namespace; Added MVC attribute to controller; Expanded Route string; Injected Logic interface; Added dtos to endpoint, made asynchronous & changed from Get to Post to allow for request body

--- Moonpig.PostOffice.Api/Controllers/DespatchDateController.cs
+++ Moonpig.PostOffice.Api/Controllers/DespatchDateController.cs
@@ -1,35 +1,27 @@
-﻿namespace Moonpig.PostOffice.Api.Controllers
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Data;
-    using Microsoft.AspNetCore.Mvc;
-    using Model;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moonpig.PostOffice.Infrastructure.BusinessLogic;
+using Moonpig.PostOffice.Model.Dto;
 
-    [Route("api/[controller]")]
+namespace Moonpig.PostOffice.Api.Controllers
+{
+    [ApiController]
+    [Route("post-office-api/[controller]/[action]")]
     public class DespatchDateController : Controller
     {
-        public DateTime _mlt;
+        private readonly IDespatchLogic _despatchLogic;
 
-        [HttpGet]
-        public DespatchDate Get(List<int> productIds, DateTime orderDate)
+        public DespatchDateController(IDespatchLogic despatchLogic)
         {
-            _mlt = orderDate; // max lead time
-            foreach (var ID in productIds)
-            {
-                DbContext dbContext = new DbContext();
-                var s = dbContext.Products.Single(x => x.ProductId == ID).SupplierId;
-                var lt = dbContext.Suppliers.Single(x => x.SupplierId == s).LeadTime;
-                if (orderDate.AddDays(lt) > _mlt)
-                    _mlt = orderDate.AddDays(lt);
-            }
-            if (_mlt.DayOfWeek == DayOfWeek.Saturday)
-            {
-                return new DespatchDate { Date = _mlt.AddDays(2) };
-            }
-            else if (_mlt.DayOfWeek == DayOfWeek.Sunday) return new DespatchDate { Date = _mlt.AddDays(1) };
-            else return new DespatchDate { Date = _mlt };
+            _despatchLogic = despatchLogic;
+        }
+
+        [HttpPost]
+        public async Task<DespatchDateResponseDto> Get([FromBody] DespatchDateRequestDto request)
+        {
+            return await _despatchLogic.GetDespatchDateAsync(request);
         }
     }
 }
