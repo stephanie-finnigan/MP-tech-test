5fdceb13ba04fe471ed7e937272fc000bda60b95
Stephanie Finnigan <snoellm@gmail.com>
2023-12-14 18:29:01 +0000

    Changed method name for clarity

--- Moonpig.PostOffice.Infrastructure/DataAccess/OrderQuery.cs
+++ Moonpig.PostOffice.Infrastructure/DataAccess/OrderQuery.cs
@@ -7,7 +7,7 @@ namespace Moonpig.PostOffice.Infrastructure.DataAccess
 {
     public interface IOrderQuery
     {
-        Task<int> GetOrderSupplierLeadTime(int productId);
+        Task<int> GetSupplierLeadTimeAsync(int productId);
     }
 
     public class OrderQuery : IOrderQuery
@@ -19,7 +19,7 @@ namespace Moonpig.PostOffice.Infrastructure.DataAccess
             _dbContext = dbContext;
         }
 
-        public Task<int> GetOrderSupplierLeadTime(int productId)
+        public Task<int> GetSupplierLeadTimeAsync(int productId)
         {
             var query = (from p in _dbContext.Products
                          join s in _dbContext.Suppliers
