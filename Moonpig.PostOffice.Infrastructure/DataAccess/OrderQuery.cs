08890b06ff3f9f4ef24f3db7936987df7d4cbb7a
Stephanie Finnigan <snoellm@gmail.com>
2023-12-14 12:28:59 +0000

    Added new folder & class for DataAccess
    Created new interface & class OrderQuery; Injected DbContext interface into class

---
+++ Moonpig.PostOffice.Infrastructure/DataAccess/OrderQuery.cs
@@ -0,0 +1,33 @@
+﻿using Moonpig.PostOffice.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonpig.PostOffice.Infrastructure.DataAccess
+{
+    public interface IOrderQuery
+    {
+        Task<int> GetOrderSupplierLeadTime(int productId);
+    }
+
+    public class OrderQuery : IOrderQuery
+    {
+        private readonly IDbContext _dbContext;
+
+        public OrderQuery(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<int> GetOrderSupplierLeadTime(int productId)
+        {
+            DbContext dbContext = new DbContext();
+            var s = dbContext.Products.Single(x => x.ProductId == ID).SupplierId;
+            var lt = dbContext.Suppliers.Single(x => x.SupplierId == s).LeadTime;
+
+            return null;
+        }
+    }
+}
