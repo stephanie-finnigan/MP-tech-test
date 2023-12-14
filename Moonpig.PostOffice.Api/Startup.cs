c2f2b5c0fa033d628e267b7af11dce3df3fc1890
Stephanie Finnigan <snoellm@gmail.com>
2023-12-14 18:28:08 +0000

    Moved using statements; Added required services
    Added DI for interfaces/classes; Added Swagger for local testing

--- Moonpig.PostOffice.Api/Startup.cs
+++ Moonpig.PostOffice.Api/Startup.cs
@@ -1,35 +1,61 @@
-﻿namespace Moonpig.PostOffice.Api
-{
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Moonpig.PostOffice.Data;
+using Moonpig.PostOffice.Infrastructure.BusinessLogic;
+using Moonpig.PostOffice.Infrastructure.DataAccess;
 
+namespace Moonpig.PostOffice.Api
+{
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Moonpig Post Office",
+                    Version = "v1",
+                    Description = "An ASP.NET Core Web Api for processing orders from suppliers."
+                });
+            });
+            services.AddTransient<IDespatchLogic, DespatchLogic>();
+            services.AddTransient<IOrderQuery, OrderQuery>();
+            services.AddTransient<IDbContext, DbContext>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger(options =>
+            {
+                options.SerializeAsV2 = true;
+            });
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("./v1/swagger.json", "Moonpig Post Office ");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
