c6467dc4c563f5c5e8ecb574d0dfae7411f2f635
Stephanie Finnigan <snoellm@gmail.com>
2023-12-15 11:59:59 +0000

    Add param for setup returned lead time

--- Moonpig.PostOffice.Tests/PostOfficeTests.cs
+++ Moonpig.PostOffice.Tests/PostOfficeTests.cs
@@ -150,17 +150,17 @@ namespace Moonpig.PostOffice.Tests
         }
 
         [Theory]
-        [InlineData(9, "2018-01-05", "2018-01-15")]
-        [InlineData(10, "2018-01-05", "2018-01-22")]
-        public async Task LeadTimeOverMultipleWeeks(int productId, DateTime orderDate, DateTime expOrderDate)
+        [InlineData(9, "2018-01-05", 6, "2018-01-15")]
+        [InlineData(10, "2018-01-05", 11,  "2018-01-22")]
+        public async Task LeadTimeOverMultipleWeeks(int prodId, DateTime orderDate, int lt, DateTime expOrderDate)
         {
             // Given
             _requestDto = new OrderRequestDto
             {
-                ProductIds = new List<int> { productId },
+                ProductIds = new List<int> { prodId },
                 OrderDate = orderDate
             };
-            _orderQueryMock.Setup(q => q.GetSupplierLeadTimeAsync(It.IsAny<List<int>>())).ReturnsAsync(1);
+            _orderQueryMock.Setup(q => q.GetSupplierLeadTimeAsync(It.IsAny<List<int>>())).ReturnsAsync(lt);
 
             // When
             _responseDto = await _logic.GetDespatchDateAsync(_requestDto);
